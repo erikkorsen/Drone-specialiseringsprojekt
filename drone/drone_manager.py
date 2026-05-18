b0293e10d036c7f6dda66ca841c2cdd21a7ca9c2
erikkorsen <erik.korsen@gmail.com>
2026-05-18 13:31:34 +0200

    demo knapp

--- drone/drone_manager.py
+++ drone/drone_manager.py
@@ -22,6 +22,17 @@ home_coords_3 = (13.192135, 55.695985)
 
 homes = [home_coords, home_coords_2, home_coords_3]
 
+demo_missions = [
+    ((13.1901382, 55.6978476), (13.1886, 55.7075)),
+    ((13.1882112, 55.7067047), (13.1860, 55.7048)),
+    ((13.1636491, 55.7164838), (13.2305, 55.6995)),
+    ((13.19778, 55.70191), (13.1995, 55.7140)),
+    ((13.1901382, 55.6978476), (13.2350, 55.7165)),
+    ((13.1882112, 55.7067047), (13.1912, 55.7038)),
+    ((13.1636491, 55.7164838), (13.2035, 55.7095)),
+    ((13.19778, 55.70191), (13.1880, 55.7080)),
+]
+
 def create_drones(count=10):
 
     drones = {}
@@ -154,6 +165,32 @@ def handle_mission():
     thread.start()
 
     return {"message": "Mission started", "drone_id": drone.id}, 200
+
+@app.route("/demo", methods=["POST"])
+def start_demo():
+
+    for mission in demo_missions:
+        from_coords = mission[0]
+        to_coords = mission[1]
+
+        drone = get_idle_drone(from_coords, to_coords)
+
+        if drone is None:
+            continue
+
+        drone.assign_mission(from_coords, to_coords)
+        sync_drone(drone)
+
+        thread = threading.Thread(
+            target = run_mission,
+            args = (drone, from_coords, to_coords),
+            daemon = True,
+        )
+        thread.start()
+
+        time.sleep(2)
+    
+    return {"message": "Demo started"}, 200
 
 
 if __name__ == "__main__":
